ffddde3e3d809a11a522cae9b0167667c67cad1c
raghuveerdhayal <raghuveerdhayal98@gmail.com>
2025-09-18 20:50:34 +0530

    Add shoet code genration begore create url

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,4 +3,13 @@ class Url < ApplicationRecord
   validates :original_url, presence: true, format: URI::regexp(%w[http https])
   validates :short_code, uniqueness: true
 
+  before_create :generate_short_code
+
+  def generate_short_code
+    loop do
+      self.short_code = SecureRandom.alphanumeric(6)
+      break unless Url.exists?(short_code: short_code)
+    end
+  end
+
 end
